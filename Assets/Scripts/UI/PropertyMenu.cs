20cae2ce67f38583b091eeb5948d4c82c282d9ba
FireAndy <asvea2003@yahoo.dk>
2019-04-11 15:20:40 +0200

    PropertyNames

--- Assets/Scripts/UI/PropertyMenu.cs
+++ Assets/Scripts/UI/PropertyMenu.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PropertyMenu : MonoBehaviour
 {
+    public static string[] relationsNaming = {"is related to ","External URL ","form of ","is a ","part of ","has a ","used for ","capable of ","at location ","causes ","has subevent ","has first subevent ","has last subevent ","has prerequisite ","has property ","motivated by goal ","obstructed by ","desires ","created by ","synonym ","antonym ","distinct from ","derived from ","symbol of ","defined as ","entails ","manner of ","located near ","has context ","similar to ","etymologically related to ","etymologically derived from ","causes desire ","made of ","receives action ","instance of "};
+
     public PropertyContainer PropertyContainer;
 
     public ConceptNetProperty propertyMenuButton;
     // Start is called before the first frame update
     void Start()
     {
-        foreach(string x in PropertyContainer.relationURIs){
+        foreach(string x in relationsNaming){
             ConceptNetProperty y = Instantiate(propertyMenuButton, Vector3.zero, Quaternion.identity, transform);
             y.SetProperty(x);
             y.isActive = false;
