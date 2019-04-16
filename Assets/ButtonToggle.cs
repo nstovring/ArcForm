69c029fba508db81d76d0bcc8396445506ba7afb
FireAndy <asvea2003@yahoo.dk>
2019-04-16 14:35:21 +0200

    HoverGUI
    ShittyShitShiit

---
+++ Assets/ButtonToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonToggle : MonoBehaviour
+{
+    public bool toggled;
+
+    public Animator animator;
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(animator == null){
+        animator = GetComponent<Animator>();
+        }
+        if(button == null){
+        button = GetComponent<Button>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void TaskOnClick()
+    {
+        toggled = !toggled;
+        if(toggled == true){
+            //animator.GetParameter(4) = true;
+        }
+        if(toggled != true){
+        }
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button!");
+    }
+}
